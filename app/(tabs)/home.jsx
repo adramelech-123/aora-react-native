2adfa09aec8373a8c7f5d8906ef737c3dc848d6a
adramelech-123 <tmamvuto6@gmail.com>
2024-06-14 22:11:47 +0200

    User Profile Page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,10 +8,12 @@ import { useState } from 'react'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const {data: posts, refetch} = useAppwrite(getAllPosts)
   const { data: latestPosts} = useAppwrite(getLatestPosts);
 
@@ -38,10 +40,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome back
+                  Welcome back,
                 </Text>
-                <Text className="text-2xl font-psemibold text-white">
-                  Lex Luthor
+                <Text className="text-2xl font-psemibold text-secondary">
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
